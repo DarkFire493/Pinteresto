9b06e2c3ec3e63bf0d7724375152bcebdf50e13a
Abraham Rodriguez <darkfire493@gmail.com>
2017-06-28 13:29:33 -0500

    Fixed AdminUser and Devise permitions

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20170615211902) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
